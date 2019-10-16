b7d0845ab8228d00b52196e296fed11ab1f632e9
Nate Grobe <nategrobe@system76-pc.localdomain>
2019-10-15 20:56:04 -0400

    minor updates

--- pixel_art_gen.py
+++ pixel_art_gen.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import os
 
 def merge_pixels(s1, s2):
     new_values = []
@@ -6,9 +7,9 @@ def merge_pixels(s1, s2):
     G = 0
     B = 0
 
-    for j in range(0,10):
-        for i in range(s1,s1+10):
-            new_values.append(pixel_values[i])
+    for w in range(0,10):
+        for h in range(s1,s1+10):
+            new_values.append(pixel_values[h])
         s1 += 300
 
     for i in new_values:
@@ -22,27 +23,30 @@ def merge_pixels(s1, s2):
 
     avg = (R,G,B)
 
-    for j in range(0,10):
-        for i in range(s2,s2+10):
-            pixel_values[i] = avg
+    for w in range(0,10):
+        for h in range(s2,s2+10):
+            pixel_values[h] = avg
         s2 += 300
 
-
 if __name__ == "__main__":
-    cs = 0
-
-    img = Image.open('bird.jpg','r')
+    img = Image.open('bird.jpg')
     pixel_values = list(img.getdata())
-    width, height = img.size
+    width,height = img.size
+    cs = 0
+    rows = height//10 + 1
+    columns = width//10
+    row_width = width*10
+    file_types = [".jpg", ".png"]
 
-    for x in range(1,31):
-        for i in range(0,30):
+
+    for r in range(1,rows):
+        for c in range(0,columns):
             merge_pixels(cs,cs)
             cs+= 10
-        cs = x*3000
+        cs = r*row_width
 
-# Puts image back together and creates new image
+    # Puts image back together and creates new image
     img2 = Image.new(img.mode, img.size)
     img2.putdata(pixel_values)
     img2.show()
-    img2.save('bird2.png')
+    img2.save('pixel_art.png')
