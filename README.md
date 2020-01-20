18f49c7cd9b5906dc35d3902767d118f10d4ca0b
Nate Grobe <natepgrobe@gmail.com>
2020-01-20 14:27:00 -0500

    updates and reorganized files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pixel Art Generator
+
+Takes an image and generates a pixelated version. Pixels are 10x10 squares. Each 10x10 square is the average colour of the original 100 pixels it is replacing.
--- pixel_art_gen.py
+++ pixel_art_gen.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 from PIL import Image
 import os
 
+# gets 10x10 squares of pixels, averages their colour and replaces those 100 pixels with 100 new pixels of the new colour
 def merge_pixels(s1, s2,w):
     new_values = []
     R = 0
@@ -12,10 +15,10 @@ def merge_pixels(s1, s2,w):
             new_values.append(pixel_values[h])
         s1 += w
 
-    for i in new_values:
-        R += i[0]
-        G += i[1]
-        B += i[2]
+    for value in new_values:
+        R += value[0]
+        G += value[1]
+        B += value[2]
 
     R = round(R/100)
     G = round(G/100)
@@ -29,22 +32,34 @@ def merge_pixels(s1, s2,w):
         s2 += w
 
 if __name__ == "__main__":
-    img = Image.open('zelda.jpg')
+    # choose an image
+    imgs = os.listdir("./imgs")
+    print("Choose an image by typing its number: ")
+
+    for i in range(0, len(imgs)):
+        print(f"{i+1}. {imgs[i]}")
+
+    choice = int(input('> '))
+    img_choice = imgs[choice - 1]
+    img = Image.open(f"./imgs/{img_choice}")
+
+    # calculates number of rows and columns for new pixels
     pixel_values = list(img.getdata())
     width,height = img.size
-    cs = 0
     rows = height//10 + 1
     columns = width//10
     row_width = width*10
 
+    # loops through the image calling the merge_pixels function
+    cs = 0
     for r in range(1,rows):
         for c in range(0,columns):
             merge_pixels(cs,cs,width)
             cs+= 10
         cs = r*row_width
 
-    # Puts image back together and creates new image
+    # Puts image back together, displays, and creates new image
     img2 = Image.new(img.mode, img.size)
     img2.putdata(pixel_values)
     img2.show()
-    img2.save('pixel_art.png')
+    img2.save(f'{img_choice.split(".")[0]}.png')
